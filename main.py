83748f6f048788a4a8e5d4827725a2e34b53f0d6
siuhangw <wshiahb@gmail.com>
2023-05-10 01:09:00 +0800

    Basic optimization

--- main.py
+++ main.py
@@ -1,57 +1,87 @@
+import datetime
 import pandas as pd
+import yfinance as yf
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
-# Data In
-# full_path
-# relative_path
-df = pd.read_csv('0388.HK.csv')
-df = df[['Date', 'Open', 'High', 'Low', 'Close']]
+def backtest(df, candle_len, stop_win, stop_loss):
 
-df['Range'] = df['High'] - df['Low']
-df['Pct_Range'] = df['Range'] - df['Open']
-df['Barsize'] = df['Close'] - df['Open']
-df['Pct_Barsize'] = df['Barsize'] / df['Open']
-df['Pct_Change'] = df['Close'].pct_change()
-print(df.head(5))
+    ### Initialization
+    pos_opened = False
+    open_date = datetime.datetime.now().date()
+    open_price = 0
+    close_price = 0
+    pnl = 0
+    pnl_list = []
 
-# Initialization
-pos_opened = False
-open_price = 0
-close_price = 0
-pnl = 0
-pnl_list = []
+    ### Backtest
+    for i, row in df.iterrows():
+        now_date = i.date()
+        now_open = row['Open']
+        now_high = row['High']
+        now_low = row['Low']
+        now_close = row['Close']
+        now_candle = round(now_close - now_open, 2)
+        now_barsize = row['Pct_Barsize']
+        now_pct_change = row['Pct_Change']
 
-# Backtest
-for i in range(len(df)):
-    now_date = df.loc[(i, 'Date')]
-    now_open = df.loc[(i, 'Open')]
-    now_high = df.loc[(i, 'High')]
-    now_low = df.loc[(i, 'Low')]
-    now_close = df.loc[(i, 'Close')]
-    now_barsize = df.loc[(i, 'Pct_Barsize')]
-    now_pct_change = df.loc[(i, 'Pct_Change')]
+        # Trade Logic
+        open_pos_cond = now_candle > candle_len
+        close_pos_cond = (now_date - open_date).days >= 7
+        stop_win_cond = now_close - open_price > stop_win
+        stop_loss_cond = now_close - open_price < stop_loss
+        last_index_cond = i == df.index[-1]
 
-    # Open Position
-    # if (pos_opened == False) and ((now_close - now_open) < -0.03 * now_open):
-    if (pos_opened == False) and (now_pct_change < -0.03):
-        pos_opened = True
-        open_price = now_close
+        # Open Position
+        if (pos_opened == False) and open_pos_cond:
+            pos_opened = True
+            open_price = now_close
+            open_date = now_date
 
-    # Close Position (Profit Taking & Stop Loss)
-    if (pos_opened == True) and ((now_close - open_price > 0.03 * open_price) or (now_close - open_price < -0.02 * open_price) or (i == len(df) - 1)):
-        pos_opened = False
-        close_price = now_close
-        pnl = close_price - open_price
-        pnl_list.append(pnl)
+        # Close Position
+        elif (pos_opened == True) and (stop_win_cond or stop_loss_cond or last_index_cond or close_pos_cond):
+            pos_opened = False
+            close_price = now_close
+            pnl = close_price - open_price
+            pnl_list.append(pnl)
+
+    ### Result
+    total_profit = sum(pnl_list)
+    num_of_trade = len(pnl_list)
+    avg_pnl = total_profit / num_of_trade
+
+    print('total_profit: ', total_profit)
+    print('num_of_trade: ', num_of_trade)
+    print('avg_pnl: ', avg_pnl)
+    print('########################')
 
 
-total_profit = sum(pnl_list)
-num_of_trade = len(pnl_list)
-avg_pnl = total_profit / num_of_trade
+if __name__ == '__main__':
 
-print(total_profit)
-print(num_of_trade)
-print(avg_pnl)
+    ### Data In
+    # full_path
+    # relative_path
+    ticker = yf.Ticker('0388.HK')
+    df = ticker.history(start='2022-01-01', end='2022-12-31')
+
+    ### Data Cleaning
+    df = df[['Open', 'High', 'Low', 'Close', 'Volume']]
+    df = df[df['Volume'] > 0]
+    df['Range'] = df['High'] - df['Low']
+    df['Pct_Range'] = df['Range'] - df['Open']
+    df['Barsize'] = df['Close'] - df['Open']
+    df['Pct_Barsize'] = df['Barsize'] / df['Open']
+    df['Pct_Change'] = df['Close'].pct_change()
+
+    ### Parameters Combination
+    candle_len_list = [5, 10, 15]
+    stop_win_list = [2, 3]
+    stop_loss_list = [20, 30]
+
+    ### Iteration
+    for stop_loss in stop_loss_list:
+        for stop_win in stop_win_list:
+            for candle_len in candle_len_list:
+                backtest(df, candle_len, stop_win, stop_loss)
